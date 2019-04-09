6cb4ede655bdcb568f538961e09f6b1e24d7dbf4
LisaCee <lisaceedesign@gmail.com>
2019-04-08 20:32:30 -0700

    sidebar for mobile good, work on desktop

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,31 +1,58 @@
-import React from "react";
+import React, { Component } from "react";
+import "../styles/sidebar.css";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink
+} from "reactstrap";
 
-const Sidebar = () => {
-  return (
-    <div>
-      <nav>
-        <ul>
-          <li className="brand">
-            <a href="#">jaw</a>
-          </li>
-          <li>
-            <a href="#">Art</a>
-          </li>
-          <li>
-            <a href="#">About</a>
-          </li>
-          <li>
-            <a href="#">Shop</a>
-          </li>
-          <li>
-            <a href="#">
-              <i className="fab fa-instagram fa-2x" />
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  );
-};
+class Sidebar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+       <Navbar color="light" light expand="md" className="sidebar">
+          <NavbarBrand className="nav-header brand" href="/">jaw</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="#">Street Art</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#">Fine Art</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#">Store</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#">About</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#"><i className="fab fa-instagram fa-2x"></i></NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
 
 export default Sidebar;
