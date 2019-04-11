6423c38c4b8011a81e3bf32c01240a26f838803f
LisaCee <lisaceedesign@gmail.com>
2019-04-11 16:41:31 -0700

    tweaked sidebar style and added main img

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,9 +1,20 @@
 import React from "react";
+import "../styles/main.css";
+import { Container, Row, Col } from "reactstrap";
 
 const Main = () => {
   return (
     <div>
-      <img src="http://placekitten.com/200/300" alt="placeholder" />
+      <Container>
+        <Row>
+          <Col>
+            <img
+              src={require("../images/JAWheart.jpg")}
+              alt="concrete lego hearts"
+            />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,16 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./styles.css";
-import { Container, Row, Col } from "reactstrap";
+
 import Sidebar from "./Components/Sidebar";
 import Main from "./Components/Main";
 
 function App() {
   return (
     <div className="App">
-      
-        <Sidebar />
-      
+      <Sidebar />
+      <Main />
     </div>
   );
 }
